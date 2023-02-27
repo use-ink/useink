4a4317e0e49c9120042278e88822d338f6a38d26
Sam Ruberti <samruberti@gmail.com>
2023-02-27 17:24:42 -0500

    chore: Misc cleanup

--- core/hooks/useBlockHeader.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { BlockHeaderContext } from '../providers/blockHeader/mod.ts';
-
-export type { BlockHeader } from '../providers/blockHeader/mod.ts';
-
-export const useBlockHeader = () => useContext(BlockHeaderContext);
