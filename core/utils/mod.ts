fcacf5b24f3e47a13780ff981c08edd046fb0048
Sam Ruberti <samruberti@gmail.com>
2023-03-01 14:16:49 -0500

    chore: remove nanoid

--- core/utils/mod.ts
+++ core/utils/mod.ts
@@ -2,3 +2,4 @@ export * from './contractTxUtils.ts';
 export * from './contracts/mod.ts';
 export * from './getExpiredItem.ts';
 export * from './parseUnits.ts';
+export * from './pseudoRandomId.ts';
