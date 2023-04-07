1c418ad9463ffa8cee5a7cd944656ed221947f10
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-04-07 18:29:24 +0300

    feat: utils - bn.js utility wrappers.

--- core/utils/mod.ts
+++ core/utils/mod.ts
@@ -1,5 +1,9 @@
+import * as dotUtils from '@polkadot/util';
+
+export { dotUtils };
+
 export * from './contractTxUtils.ts';
 export * from './contracts/mod.ts';
 export * from './getExpiredItem.ts';
-export * from './parseUnits.ts';
 export * from './pseudoRandomId.ts';
+
