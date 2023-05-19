02a13749e9d25742f081cb04fa62072369f36418
Sam Ruberti <samruberti@gmail.com>
2023-05-19 17:05:48 -0400

    feat: add RustResult type to utils

--- utils/mod.ts
+++ utils/mod.ts
@@ -1,5 +1,6 @@
 export * from '@polkadot/util';
 
+export * from './types/mod.ts';
 export * from './pickDecoded.ts';
 export * from './pickError.ts';
 export * from './pickResultErr.ts';
---
+++ utils/types/common.ts
@@ -0,0 +1,1 @@
+export type RustResult<T, E> = { Ok?: T; Err?: E };
