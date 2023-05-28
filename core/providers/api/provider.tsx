359c4336ef9589b3eba392ae43d40a145900bd50
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-05-28 08:35:48 +0300

    Add Smoldot providers on init.

--- core/providers/api/provider.tsx
+++ core/providers/api/provider.tsx
@@ -1,30 +1,52 @@
-import React, { useEffect, useReducer } from 'react'
-import { ApiPromise, WsProvider } from '@polkadot/api'
-import { useChains } from '../../hooks/mod.ts'
-import { APIContext } from './context.ts'
-import { apiProvidersReducer } from './reducer.ts'
-import { useConfig } from '../../mod.ts'
+import React, { useEffect, useReducer } from "react";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ScProvider } from "@polkadot/rpc-provider/substrate-connect";
+import * as Sc from "@substrate/connect";
+import { useChains } from "../../hooks/mod.ts";
+import { APIContext } from "./context.ts";
+import { apiProvidersReducer } from "./reducer.ts";
+import { useConfig } from "../../mod.ts";
+import { Chain, ChainId } from "../../../chains/mod.ts";
 
 export const APIProvider: React.FC<React.PropsWithChildren<any>> = ({
   children,
 }) => {
-  const chains = useChains()
-  const { chainRpcs } = useConfig()
-  const [apis, dispatch] = useReducer(apiProvidersReducer, {})
+  const chains = useChains();
+  const { chainRpcs } = useConfig();
+  const [apis, dispatch] = useReducer(apiProvidersReducer, {});
+
+  const addApiProvider = (chain: Chain) => {
+    const provider = new WsProvider(chainRpcs[chain.id] || chain.rpcs[0]);
+
+    ApiPromise.create({ provider }).then((api) => {
+      dispatch({
+        type: "ADD_API_PROVIDER",
+        chainId: chain.id,
+        apiProvider: { api, provider },
+      });
+    });
+  };
+
+  const addSmoldotProvider = async (chain: Chain) => {
+    const provider = new ScProvider(Sc, chain.id);
+    await provider.connect();
+    const api = await ApiPromise.create({ provider });
+
+    ApiPromise.create({ provider }).then((api) => {
+      dispatch({
+        type: "ADD_SMOLDOT_PROVIDER",
+        chainId: chain.id,
+        apiProvider: { api, provider },
+      });
+    });
+  };
 
   useEffect(() => {
     chains.forEach((chain) => {
-      const provider = new WsProvider(chainRpcs[chain.id] || chain.rpcs[0])
+      addApiProvider(chain);
+      addSmoldotProvider(chain);
+    });
+  }, [chains, chainRpcs]);
 
-      ApiPromise.create({ provider }).then((api) => {
-        dispatch({
-          type: 'ADD_API_PROVIDER',
-          chainId: chain.id,
-          apiProvider: { api, provider },
-        })
-      })
-    })
-  }, [chains, chainRpcs])
-
-  return <APIContext.Provider value={{ apis }} children={children} />
-}
+  return <APIContext.Provider value={{ apis }} children={children} />;
+};
--- core/providers/api/reducer.ts
+++ core/providers/api/reducer.ts
@@ -2,7 +2,7 @@ import { ChainId } from '../../../chains/mod.ts';
 import { IApiProvider, IApiProviders } from './model.ts';
 
 interface AddApiProvider {
-  type: 'ADD_API_PROVIDER';
+  type: 'ADD_API_PROVIDER' | 'ADD_SMOLDOT_PROVIDER'
   chainId: ChainId;
   apiProvider: IApiProvider;
 }
@@ -19,5 +19,11 @@ export function apiProvidersReducer(
         ...state,
         [action.chainId]: action.apiProvider,
       };
+
+      case 'ADD_SMOLDOT_PROVIDER':
+        return {
+          ...state,
+          [action.chainId]: action.apiProvider,
+        };
   }
 }
