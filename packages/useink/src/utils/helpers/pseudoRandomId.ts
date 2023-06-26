5aa9d56ae6b7396206ee711d42726aa6d6c4f36d
Sam Ruberti <samruberti@gmail.com>
2023-06-26 16:19:07 -0400

    fix: remove crypto dependency for browser compatibility (#95)

--- packages/useink/src/utils/helpers/pseudoRandomId.ts
+++ packages/useink/src/utils/helpers/pseudoRandomId.ts
@@ -1,15 +1,3 @@
-export const pseudoRandomId = (t = 21) =>
-  crypto.getRandomValues(new Uint8Array(t)).reduce(
-    (t, e) =>
-      // rome-ignore lint: fancy code
-      (t +=
-        // rome-ignore lint: fancy code
-        (e &= 63) < 36
-          ? e.toString(36)
-          : e < 62
-          ? (e - 26).toString(36).toUpperCase()
-          : e > 62
-          ? '-'
-          : '_'),
-    '',
-  );
+import { nanoid } from 'nanoid';
+
+export const pseudoRandomId = (t = 21) => nanoid(t);
