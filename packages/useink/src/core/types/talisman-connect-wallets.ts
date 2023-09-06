f1b127cfafa653ecf1e53baa5a0c0b44a33a9758
Sam Ruberti <samruberti@gmail.com>
2023-09-06 14:59:36 -0400

    fix: wallet account type (#115)

--- packages/useink/src/core/types/talisman-connect-wallets.ts
+++ packages/useink/src/core/types/talisman-connect-wallets.ts
@@ -1,8 +1,12 @@
 export { getWalletBySource, getWallets } from '@talismn/connect-wallets';
+import type { Wallet } from '@talismn/connect-wallets';
 import { Signer } from './api';
-import type { WalletAccount as TalismanWalletAccount } from '@talismn/connect-wallets';
 
-export interface WalletAccount extends TalismanWalletAccount {
+export interface WalletAccount {
   // Talisman sets the type as unknown so we must manually set it to Signer
   signer?: Signer;
+  address: string;
+  source: string;
+  name?: string;
+  wallet?: Wallet;
 }
